20a4145781b86dd9b542d8d6816ccb7c50986df0
hectopascal <yn2161@nyu.edu>
2026-05-10 11:20:24 +0000

    Deploying to gh-pages from @ hectopascal/hectopascal.github.io@b0a6cd95e33fb37a529065a7f8a8fc96cd60f685 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "hectopascal/yiyanng.github.io",
+    "data-repo": "hectopascal/hectopascal.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
